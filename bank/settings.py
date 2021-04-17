1d696e756ddefa7ca17258d35ca48968fd61d452
chinmay <agrawalchinmay84@gmail.com>
2021-04-17 18:12:09 +0530

    settings

--- bank/settings.py
+++ bank/settings.py
@@ -125,11 +125,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / 'static',
+# ]
 
-# STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 LOGOUT_REDIRECT_URL = 'home'
 
